6cadc46dca39327f851d3abd4e4005d9d43d7e14
okadath <vintaw.01@gmail.com>
2021-12-06 01:29:27 -0600

    funciona para strings

--- lexico.py
+++ lexico.py
@@ -8,6 +8,7 @@ def crea_tabla_tansiciones():
 	graph[(1, "(")] = 13
 	graph[(1, ")")] = 14
 	graph[(1, "l")] = 7
+	# graph[(1, "")] = 7
 
 	graph[(7, "d")] = 7
 	graph[(7, "l")] = 7
@@ -26,7 +27,8 @@ def crea_tabla_tansiciones():
 	graph[(8, "/")] = "ID"
 	graph[(8, "(")] = "ID"
 	graph[(8, ")")] = "ID"
-
+	graph[(8,"")]="ID"
+	
 	graph[(8, "l")] = "ID"
 	graph[(9, "+")] = "SUM"
 	graph[(10, "-")] = "RES"
@@ -49,47 +51,54 @@ insertados = 1
 retroceso = False
 num = ""
 apilando_letra = False
-
+dejo_de_apilar=False
+end=False
 while 1:
 
-	if retroceso:
-		file.seek(a - 2)
+	if retroceso and char!="":
+		file.seek(a - 1)
 		retroceso = False
 	char = file.read(1)
 
 	a = file.tell()
 	if not apilando_letra:
-		if not char :
+		posicion_grafo = 1
+		cadena = ""
+		if end:
 			break
+		if not char :
+			end=True
+
 	if char.isalpha():
 		buffer = "l"
 	elif char.isdigit():
 		buffer = "d"
-	# elif char=="":
-	# 	buffer=""
 	else:
-		buffer = char
-	if not apilando_letra:
-		posicion_grafo = 1
-		cadena = ""
-
+		if apilando_letra:
+			dejo_de_apilar=True
+			apilando_letra=False
+		else:
+			buffer = char
+			
 	while True:
+		print(lista_identificadores)
+		print(lista_tokens)
+		if dejo_de_apilar:
+			# posicion_grafo = tabla[(posicion_grafo, buffer)]
+			lista_tokens.append(cadena)
+			lista_identificadores.append("ID" + str(insertados))
+			apilando_letra=False
+			dejo_de_apilar=False
+			retroceso=True
+			break
 		try:
 			if str(posicion_grafo).isdigit():
 				if buffer == "l":
 					cadena = cadena + char
 					apilando_letra = True
-
-				if buffer == "\n" or buffer == " ":
-					apilando_letra = False
+				if buffer == "":
 					break
-				if char=="":
-					apilando_letra=False
-					lista_tokens.append(cadena)
-					lista_identificadores.append("ID" + str(insertados))
-					break
-
-			posicion_grafo = tabla[(posicion_grafo, buffer)]
+				posicion_grafo = tabla[(posicion_grafo, buffer)]
 
 		except Exception as e:
 			raise e
@@ -99,10 +108,8 @@ while 1:
 				lista_tokens.append(cadena)
 				apilando_letra = False
 				retroceso = True
-			# lista_identificadores.append(posicion_grafo+str(insertados))
 			else:
 				lista_tokens.append(buffer)
-		
 			lista_identificadores.append(posicion_grafo + str(insertados))
 			insertados = insertados + 1
 			posicion_grafo = 1
@@ -112,7 +119,7 @@ while 1:
 			break
 		if apilando_letra:
 			break
+		
 
 file.close()
-print(lista_identificadores)
-print(lista_tokens)
+
